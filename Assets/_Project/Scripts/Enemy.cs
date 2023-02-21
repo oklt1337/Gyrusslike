2165d5f8a346c5c2f0e6658578230f81b6e815df
Christopher Zelch <71003691+oklt1337@users.noreply.github.com>
2023-02-21 18:37:42 +0100

    Task 2 Done

---
+++ Assets/_Project/Scripts/Enemy.cs
@@ -0,0 +1,57 @@
+using System;
+using UnityEngine;
+
+namespace _Project.Scripts
+{
+    public class Enemy : MonoBehaviour
+    {
+        [SerializeField] private float angularSpeed = 2f;
+        [SerializeField] private float circleRad = .1f;
+        [SerializeField] private float circleRadMultiplier = .01f;
+        
+        private readonly Vector3 _fixedPoint = Vector3.zero;
+        private float _currentAngle;
+        private float _angularSpeed;
+        private const float MaxCircleRad = 11.0f;
+
+        public event Action<Enemy> OnDeath;
+
+        public void Init(int startDirection)
+        {
+            _angularSpeed = angularSpeed * startDirection;
+        }
+        
+        private void Update()
+        {
+            if (circleRad >= MaxCircleRad)
+                Die();
+            
+            Move();
+            IncreaseRadius();
+        }
+        
+        private void Move()
+        {
+            _currentAngle += _angularSpeed * Time.deltaTime;
+            var offset = new Vector2 (Mathf.Sin (_currentAngle), Mathf.Cos (_currentAngle)) * circleRad;
+            transform.position = ((Vector2) _fixedPoint) + offset;
+        }
+
+        private void IncreaseRadius()
+        {
+            circleRad += Time.deltaTime * circleRadMultiplier;
+        }
+
+        private void Die()
+        {
+            OnDeath?.Invoke(this);
+            Destroy(gameObject);
+        }
+
+        private void OnTriggerEnter2D(Collider2D col)
+        {
+            if (col.CompareTag("Projectile"))
+                Die();
+        }
+    }
+}
---
+++ Assets/_Project/Scripts/EnemySpawner.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace _Project.Scripts
+{
+    public class EnemySpawner : MonoBehaviour
+    {
+        [SerializeField] private GameObject enemyPrefab;
+        [SerializeField] private Vector2Int spawnRange = new(2, 10);
+        [SerializeField] private float reSpawnDelay = 2f;
+        [SerializeField] private float spawnDelay = .2f;
+        
+        private readonly List<Enemy> _enemies = new();
+        private Coroutine _spawnCo;
+
+        private void Start()
+        {
+            _spawnCo = StartCoroutine(EnemySpawnDelayCo());
+        }
+
+        private void SpawnEnemies()
+        {
+            var enemySpawnCount = Random.Range(spawnRange.x, spawnRange.y);
+            StartCoroutine(EnemySpawnCo(enemySpawnCount, -1));
+        }
+
+        private void SpawnEnemy(int direction)
+        {
+            var enemy = Instantiate(enemyPrefab, transform.position, Quaternion.identity).GetComponent<Enemy>();
+            enemy.Init(direction);
+            enemy.OnDeath += RemoveEnemy;
+            _enemies.Add(enemy); 
+        }
+
+        private void RemoveEnemy(Enemy enemy)
+        {
+            if (_enemies.Contains(enemy))
+                _enemies.Remove(enemy);
+
+            if (_enemies.Count > 0)
+                return;
+
+            if (_spawnCo != null)
+                StopCoroutine(_spawnCo);
+            _spawnCo = StartCoroutine(EnemySpawnDelayCo());
+        }
+
+        private IEnumerator EnemySpawnDelayCo()
+        {
+            yield return new WaitForSeconds(reSpawnDelay);
+            SpawnEnemies();
+        }
+
+        private IEnumerator EnemySpawnCo(int enemySpawnCount,int direction)
+        {
+            for (var i = 0; i < enemySpawnCount; i++)
+            {
+                yield return new WaitForSeconds(spawnDelay);
+                SpawnEnemy(direction);
+            }
+        }
+    }
+}
